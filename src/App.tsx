c04bc93d44491fd6019d1ced13d7c35a07e9eabc
Shemona Singh <singhshemona@gmail.com>
2024-01-01 18:25:45 -0500

    getting all data now

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,50 @@
 import { useState } from "react";
 import { Introduction } from "./components";
 
-// type Block = {
-//   object: string;
-//   id: string;
-//   parent: {
-//     type: string;
-//     page_id: string;
-//   };
-//   created_time: string;
-//   last_edited_time: string;
-//   created_by: { object: string; id: string };
-//   last_edited_by: { object: string; id: string };
-//   has_children: boolean;
-//   archived: boolean;
-//   type: string;
-//   paragraph: {
-//     color: string;
-//     text: {
-//       plain_text: string;
-//     }[];
-//   };
-// };
-
-// type Payload = {
-//   title: string;
-//   quote: Block[];
-//   source: Block[];
-// };
-
-const App = () => {
+export const App = () => {
   const [title, setTitle] = useState<string>();
   const [quote, setQuote] = useState<string>();
   const [source, setSource] = useState<string>();
+  const [loading, setLoading] = useState<boolean>(false);
 
   return (
     <div>
       <Introduction />
       <button
         type="button"
+        disabled={loading}
         onClick={() => {
+          setLoading(true);
           fetch("http://localhost:8000/")
             .then((response) => response.json())
             .then((payload) => {
               if (payload) {
                 console.log(payload);
 
-                setTitle(payload.title.title[0].plain_text);
-                setQuote(payload.quote[0].paragraph.text[0].plain_text);
-                setSource(
-                  payload.source.properties.Medium.select.name +
+                const title = payload.title.title[0].plain_text;
+                const quote = payload.quote[0].paragraph.text[0].plain_text;
+                const source = payload.source.properties.Medium.select
+                  ? payload.source.properties.Medium.select.name +
+                    ": " +
                     payload.source.properties.Title.title[0].plain_text
-                );
+                  : payload.source.icon.emoji +
+                    ": " +
+                    payload.source.properties.Title.title[0].plain_text;
+
+                setTitle(title);
+                setQuote(quote);
+                setSource(source);
               }
-            });
+            })
+            .catch((error) => console.log(error))
+            .then(() => setLoading(false));
         }}
       >
-        Fetch List
+        {loading ? "Loading..." : "New page"}
       </button>
-      {title}
-      {quote}
-      {source}
+      {title && <p>{title}</p>}
+      {quote && <p>{quote}</p>}
+      {source && <p>{source}</p>}
     </div>
   );
 };
-
-export default App;
