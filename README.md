50e289e7d7b35c0359e0e886b7a65c5f130cb380
Shemona Singh <singhshemona@gmail.com>
2024-01-07 22:47:41 -0500

    added readme and updated into

---
+++ README.md
@@ -0,0 +1,16 @@
+## Spark
+
+Demo [project](https://spark-v2-client.vercel.app/).
+
+The data for this website comes from [this Notion page](https://shemona.notion.site/35de7cb65366432eb56d815a97a4767e?v=e456dfbbc1f64e99a4d6920c7fc2e741). 
+
+It was inspired by [the following repo](https://github.com/alexeagleson/react-node-notion) and help in deploying came from [this tutorial](https://youtu.be/CNJkX9rYI8U?si=4ZmkgQA-p4ICXmIs).
+
+### To run locally
+1. Clone repo
+2. `cd client`
+3. `npm start`
+4. Open a second terminal window
+5. Go back into root directory
+6. `cd server`
+7. `npm run start`
--- client/src/components/Introduction.tsx
+++ client/src/components/Introduction.tsx
@@ -15,22 +15,13 @@ export const Introduction = () => {
         </a>{" "}
         from my{" "}
         <a
-          href="https://www.notion.so/35de7cb65366432eb56d815a97a4767e"
+          href="https://shemona.notion.site/35de7cb65366432eb56d815a97a4767e?v=e456dfbbc1f64e99a4d6920c7fc2e741"
           target="_blank"
           rel="noreferrer"
         >
           slip-box
         </a>{" "}
-        of growing highlights. In order for it to work, you must run the server
-        by following the instructions{" "}
-        <a
-          href="https://github.com/singhshemona/spark-v2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          outlined in the repo here
-        </a>
-        . Click on the "New Spark" button for a different highlight.
+        of growing highlights. Click on the "New Spark" button for a different highlight.
       </h3>
     </>
   );
